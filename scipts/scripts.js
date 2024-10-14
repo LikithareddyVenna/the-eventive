da864dd265626dc489ad279e775a4646e75a0097
LikithareddyVenna <likithareddy2882@gmail.com>
2024-10-14 22:19:55 +0530

    Update scripts.js

--- scipts/scripts.js
+++ scipts/scripts.js
@@ -37,11 +37,5 @@ function showService(serviceId) {
     document.getElementById(serviceId).classList.add('active');
     event.target.classList.add('active');
 
-function scrollCarousel(direction) {
-    const carousel = document.querySelector('.carousel-images');
-    const scrollAmount = 300; // Adjust scroll amount as needed
-    carousel.scrollBy({
-        left: direction * scrollAmount,
-        behavior: 'smooth'
-    });
-}
+
+    
