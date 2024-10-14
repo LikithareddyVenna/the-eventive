dc1effcc02df7e293cfa9235169d54ca1d76dc8a
LikithareddyVenna <likithareddy2882@gmail.com>
2024-10-14 22:37:41 +0530

    Update carousel.js

--- scipts/carousel.js
+++ scipts/carousel.js
@@ -1,6 +1,6 @@
 function scrollCarousel(direction) {
     const carousel = document.querySelector('.carousel-images');
-    const scrollAmount = 300; // Adjust scroll amount as needed
+    const scrollAmount = carousel.clientWidth; // Adjust scroll amount as needed
     carousel.scrollBy({
         left: direction * scrollAmount,
         behavior: 'smooth'
